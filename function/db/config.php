88cafbf307b28aa5d76255dc90ccc488d93bbe68
dev-pinza <pinzastudio2020@gmail.com>
2021-06-09 11:24:10 +0100

    PUSHING ONLINE

--- function/db/config.php
+++ function/db/config.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = "localhost"; /* Host name */
-$user = "root"; /* User */
-$password = ""; /* Password */
-$dbname = "nspire_db"; /* Database name */
+$user = "nspireco_core"; /* User */
+$password = "MceFd8^P%o.;"; /* Password */
+$dbname = "nspireco_nspire"; /* Database name */
 
 $con = mysqli_connect($host, $user, $password,$dbname);
 // Check connection
